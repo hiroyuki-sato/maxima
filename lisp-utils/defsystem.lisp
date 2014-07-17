46fd68a65c9e3f383277aa0033f1fba8ec9405ad
Camm Maguire <camm@debian.org>
2014-07-17 21:17:23 +0000

    mime support, gcl dlopen support, emacs keybinding change
    Gbp-Pq: 5.29-refresh.

--- lisp-utils/defsystem.lisp
+++ lisp-utils/defsystem.lisp
@@ -4671,6 +4671,7 @@ the system definition, if provided."
 			  source-pname
 			  :output-file
 			  output-file
+			  #+gcl :system-p #+gcl t
 			  #+(or :cmu :scl) :error-file
 			  #+(or :cmu :scl) (and *cmu-errors-to-file*
 						(component-full-pathname component
