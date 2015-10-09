06961d680daf47284dc243cc45aa677cf4895faa
Camm Maguire <camm@debian.org>
2015-10-09 20:55:01 +0000

    <short summary of the patch>
    Gbp-Pq: *load-pathname*-in-proper-place.

--- doc/info/de.utf8/maxima-index.lisp
+++ doc/info/de.utf8/maxima-index.lisp
@@ -2673,4 +2673,4 @@
 ("Zufallszahlen" . ("maxima.info-2" 176979 2673))
 ("Zuweisungsoperatoren" . ("maxima.info-1" 291827 9886))
 )))
-(load-info-hashtables (list (pathname-device #-gcl *load-pathname* #+gcl sys:*load-pathname*) (pathname-directory #-gcl *load-pathname* #+gcl sys:*load-pathname*)) deffn-defvr-pairs section-pairs))
+(load-info-hashtables (list (pathname-device *load-pathname*) (pathname-directory *load-pathname*)) deffn-defvr-pairs section-pairs))
