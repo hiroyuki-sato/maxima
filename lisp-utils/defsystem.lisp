85fd3991e13ef92f65119a3503eb133bba363bed
Camm Maguire <camm@debian.org>
2017-06-14 12:56:51 +0000

    <short summary of the patch>
    Gbp-Pq: pathname.

--- lisp-utils/defsystem.lisp
+++ lisp-utils/defsystem.lisp
@@ -1843,11 +1843,11 @@ s/^[^M]*IRIX Execution Environment 1, *[a-zA-Z]* *\\([^ ]*\\)/\\1/p\\
 	 (rel-directory (directory-to-list (pathname-directory rel-dir)))
 	 (rel-keyword (when (keywordp (car rel-directory))
 			(pop rel-directory)))
-         #-(or :MCL :sbcl :clisp :cmu) (rel-file (file-namestring rel-dir))
+         #-(or :MCL :gcl :sbcl :clisp :cmu) (rel-file (file-namestring rel-dir))
 	 ;; Stig (July 2001);
 	 ;; These values seems to help clisp as well
-	 #+(or :MCL :sbcl :clisp :cmu) (rel-name (pathname-name rel-dir))
-	 #+(or :MCL :sbcl :clisp :cmu) (rel-type (pathname-type rel-dir))
+	 #+(or :MCL :gcl :sbcl :clisp :cmu) (rel-name (pathname-name rel-dir))
+	 #+(or :MCL :gcl :sbcl :clisp :cmu) (rel-type (pathname-type rel-dir))
 	 (directory nil))
 
     ;; TI Common Lisp pathnames can return garbage for file names because
@@ -1873,7 +1873,7 @@ s/^[^M]*IRIX Execution Environment 1, *[a-zA-Z]* *\\([^ ]*\\)/\\1/p\\
 
     (when (and abs-name (not (null-string abs-name))) ; was abs-name
       (cond ((and (null abs-directory) (null abs-keyword))
-	     #-(or :lucid :kcl :akcl TI) (setf abs-keyword :relative)
+	     #-(or :lucid TI) (setf abs-keyword :relative)
 	     (setf abs-directory (list abs-name)))
 	    (t
 	     (setf abs-directory (append abs-directory (list abs-name))))))
@@ -1886,11 +1886,11 @@ s/^[^M]*IRIX Execution Environment 1, *[a-zA-Z]* *\\([^ ]*\\)/\\1/p\\
 	       rel-keyword)
       ;; The following feature switches seem necessary in CMUCL
       ;; Marco Antoniotti 19990707
-      #+(or :sbcl :CMU)
+      #+(or :gcl :sbcl :CMU)
       (if (typep abs-dir 'logical-pathname)
 	  (setf abs-keyword :absolute)
 	  (setf abs-keyword rel-keyword))
-      #-(or :sbcl :CMU)
+      #-(or :gcl :sbcl :CMU)
       (setf abs-keyword rel-keyword))
     (setf directory (append abs-directory rel-directory))
     (when abs-keyword (setf directory (cons abs-keyword directory)))
@@ -1900,11 +1900,11 @@ s/^[^M]*IRIX Execution Environment 1, *[a-zA-Z]* *\\([^ ]*\\)/\\1/p\\
                     :directory
                     directory
 		    :name
-		    #-(or :sbcl :MCL :clisp :cmu) rel-file
-		    #+(or :sbcl :MCL :clisp :cmu) rel-name
+		    #-(or :gcl :sbcl :MCL :clisp :cmu) rel-file
+		    #+(or :gcl :sbcl :MCL :clisp :cmu) rel-name
 
-		    #+(or :sbcl :MCL :clisp :cmu) :type
-		    #+(or :sbcl :MCL :clisp :cmu) rel-type
+		    #+(or :gcl :sbcl :MCL :clisp :cmu) :type
+		    #+(or :gcl :sbcl :MCL :clisp :cmu) rel-type
 		    ))))
 
 #-scl
