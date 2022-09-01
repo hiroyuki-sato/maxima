7bb38422fa712107a8ce27e4a1e1658f2703254e
Ubuntu Git Importer <usd-importer-do-not-mail@canonical.com>
2022-09-01 12:46:37 +0000

    New upstream version 5.37.3

--- src/autoconf-variables.lisp
+++ src/autoconf-variables.lisp
@@ -5,7 +5,7 @@
 (defparameter *autoconf-prefix* "/usr/local")
 (defparameter *autoconf-exec_prefix* "/usr/local")
 (defparameter *autoconf-package* "maxima")
-(defparameter *autoconf-version* "5.37.2")
+(defparameter *autoconf-version* "5.37.3")
 (defparameter *autoconf-libdir* "/usr/local/lib")
 (defparameter *autoconf-libexecdir* "/usr/local/libexec")
 (defparameter *autoconf-datadir* "/usr/local/share")
