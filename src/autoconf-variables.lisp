bc7f960c60963d79271d1e3325d2b6d00014e977
Ubuntu Git Importer <usd-importer-do-not-mail@canonical.com>
2022-09-01 08:47:46 +0000

    New upstream version 5.9.0

---
+++ src/autoconf-variables.lisp
@@ -0,0 +1,13 @@
+; -*- Lisp -*-
+(in-package :maxima)
+
+(defparameter *autoconf-prefix* "/home/amundson/opt")
+(defparameter *autoconf-exec_prefix* "/home/amundson/opt")
+(defparameter *autoconf-package* "maxima")
+(defparameter *autoconf-version* "5.9.0")
+(defparameter *autoconf-libdir* "/home/amundson/opt/lib")
+(defparameter *autoconf-libexecdir* "/home/amundson/opt/libexec")
+(defparameter *autoconf-datadir* "/home/amundson/opt/share")
+(defparameter *autoconf-infodir* "/home/amundson/opt/info")
+(defparameter *autoconf-host* "i686-pc-linux-gnu")
+ 
